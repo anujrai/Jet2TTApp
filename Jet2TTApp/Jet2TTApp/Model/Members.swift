f47d0b3692ad45b5276d5f7f4b3dcc8ec21c9ddb
anujrai <anuj.rai2489@gmail.com>
2020-02-22 09:04:26 +0530

    Initial commit for project setup

---
+++ Jet2TTApp/Jet2TTApp/Model/Members.swift
@@ -0,0 +1,13 @@
+//
+//  Members.swift
+//  Jet2TTApp
+//
+//  Created by Anuj Rai on 22/02/20.
+//  Copyright © 2020 Anuj Rai. All rights reserved.
+//
+
+import Foundation
+
+struct Members {
+    
+}
---
+++ Jet2TTApp/Jet2TTApp/View Controllers/Jet2TTMemberViewController.swift
@@ -0,0 +1,18 @@
+//
+//  Jet2TTMemberViewController.swift
+//  Jet2TTApp
+//
+//  Created by Anuj Rai on 22/02/20.
+//  Copyright © 2020 Anuj Rai. All rights reserved.
+//
+
+import UIKit
+
+class Jet2TTMemberViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
